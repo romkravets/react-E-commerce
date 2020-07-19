5d32c6c667e1dd23ec01691b6d14f90f632970cd
romkravets <romann.kravets@gmail.com>
2020-07-19 17:56:32 +0300

    react -app

---
+++ frontend/src/data.js
@@ -0,0 +1,41 @@
+export default  {
+    products: [
+        {
+            name: "Slom Shirt",
+            category: 'Shirt',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10
+        },
+        {
+            name: "Fit Shirt",
+            category: 'Shirt',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 4
+        },
+        {
+            name: "Slom Shirt",
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 6
+        },
+        {
+            name: "Slom Shirt",
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 9
+        },
+
+    ]
+}
