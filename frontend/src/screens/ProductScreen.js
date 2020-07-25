d2f0574ab78c655d49b473bfbf2a7a0bd396dd51
romkravets <romann.kravets@gmail.com>
2020-07-25 18:04:35 +0300

    detailsProduct

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,55 +1,71 @@
-import React from "react";
+import React, {useEffect} from "react";
 import data from "../data";
 import {Link} from "react-router-dom";
+import {useSelector, useDispatch} from "react-redux";
+import {detailsProduct} from "../actions/productActions";
 
 function ProductScreen(props) {
-    console.log(props.match.params.id);
-    const  product = data.products.find(x => x._id === props.match.params.id)
+    const productDetails = useSelector(state => state.productDetails);
+    const {product, loading, error } = productDetails;
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(detailsProduct(props.match.params.id));
+    }, []);
+
     return (
         <>
             <div className="back-to-result">
                 <Link to="/">Back to result</Link>
             </div>
-             <div className="details">
-                <div className="details-image">
-                    <img src={product.image} alt={product.name}/>
-                </div>
-                 <div className="details-info">
-                     <ul>
-                         <li>
-                             <h4>{product.name}</h4>
-                         </li>
-                         <li>
-                             {product.rating} Stars ({product.numReviews} Reviews)
-                          </li>
-                         <li>
-                             Price: <b>$ {product.price}</b>
-                         </li>
-                         <li>
-                             Description:
-                             <div>
-                                 {product.description}
-                             </div>
-                         </li>
-                     </ul>
-                 </div>
-                 <div className="details-action">
-                     <ul>
-                         <li>Price: {product.price}</li>
-                         <li>Status: {product.status}</li>
-                         <li>Qty: <select>
-                             <option>1</option>
-                             <option>2</option>
-                             <option>3</option>
-                             <option>4</option>
-                             <option>5</option>
-                         </select></li>
-                         <li>
-                             <button className="button primary">Add to Cart</button>
-                         </li>
-                     </ul>
-                 </div>
-          </div>
+            {loading ? <div>Loading...</div> :
+                error ? <div>{error}</div> :
+                    (
+                        <div className="details">
+                            <div className="details-image">
+                                <img src={product.image} alt={product.name}/>
+                            </div>
+                            <div className="details-info">
+                                <ul>
+                                    <li>
+                                        <h4>{product.name}</h4>
+                                    </li>
+                                    <li>
+                                        {product.rating} Stars ({product.numReviews} Reviews)
+                                    </li>
+                                    <li>
+                                        Price: <b>$ {product.price}</b>
+                                    </li>
+                                    <li>
+                                        Description:
+                                        <div>
+                                            {product.description}
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="details-action">
+                                <ul>
+                                    <li>Price: {product.price}</li>
+                                    <li>Status: {product.status}</li>
+                                    <li>Qty: <select>
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                    </select></li>
+                                    <li>
+                                        <button className="button primary">Add to Cart</button>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    )
+
+            }
+
         </>
     )
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,12 @@
 import {createStore, combineReducers, applyMiddleware, compose} from "redux";
-import { productListReducer } from "./reducers/productListReducer";
+import { productListReducer, productDetailsReducer } from "./reducers/productListReducer";
 import thunk from "redux-thunk";
 
 const initialState = {};
 
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer,
 });
 
 const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : (null || compose);
