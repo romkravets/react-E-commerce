4348236dfffaf2cca6ea2edf2c71d04bf1982365
romkravets <romann.kravets@gmail.com>
2020-10-31 19:51:47 +0200

    add

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,5 +1,4 @@
-import {CART_ADD_ITEM} from "../constants/cartConstants";
-import {CART_REMOVE_ITEM} from "../constants/cartConstants";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING, CART_SAVE_PAYMENT } from "../constants/cartConstants";
 
 function cartReducer(state={cartItems: []}, action) {
     switch (action.type) {
@@ -14,6 +13,10 @@ function cartReducer(state={cartItems: []}, action) {
             return {...state, cartItems: [...state.cartItems, item]};
         case CART_REMOVE_ITEM:
             return { cartItems: state.cartItems.filter(x => x.product !== action.payload) }
+        case CART_SAVE_SHIPPING:
+            return { ...state, shipping: action.payload };
+        case CART_SAVE_PAYMENT:
+            return { ...state, payment: action.payload };
         default:
             return state;
     }
